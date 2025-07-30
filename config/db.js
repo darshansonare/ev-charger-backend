bb08f22565e0c88377954c098fda3dd75fd907f7
Darshan Sonare <darshansonare@example.com>
2025-07-30 21:06:36 +0530

    Updated backend code

--- config/db.js
+++ config/db.js
@@ -1,20 +1,8 @@
-require('dotenv').config();
-const mysql = require('mysql2');
+require("dotenv").config();
+const mysql = require("mysql2");
 
-const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT
-});
+const urlDB = `mysql://${process.env.MYSQLUSER}:${process.env.MYSQLPASSWORD}@${process.env.MYSQLHOST}:${process.env.MYSQLPORT}/${process.env.MYSQLDATABASE}`;
 
-connection.connect((err) => {
-  if (err) {
-    console.error('Database connection failed:', err.stack);
-    return;
-  }
-  console.log('MySQL Connected');
-});
+const connection = mysql.createConnection(urlDB);
 
 module.exports = connection;
