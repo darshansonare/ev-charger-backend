5fdb86a46fd0e60a3f93d3a71ece11fcce3a586e
Darshan Sonare <darshansonare@example.com>
2025-07-30 19:36:35 +0530

    Updated DB config for Railway

--- config/db.js
+++ config/db.js
@@ -1,13 +1,19 @@
+require('dotenv').config();
 const mysql = require('mysql2');
+
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'Pass@123',
-  database: 'charger_app'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
 });
 
 connection.connect((err) => {
-  if (err) throw err;
+  if (err) {
+    console.error('Database connection failed:', err.stack);
+    return;
+  }
   console.log('MySQL Connected');
 });
 
