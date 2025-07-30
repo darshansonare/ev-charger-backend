b2845020e05feadab5939cf98f04c02ba46c4c33
Darshan Sonare <darshansonare@example.com>
2025-07-31 00:37:37 +0530

    Updated backend code with latest changes

--- config/db.js
+++ config/db.js
@@ -1,8 +1,25 @@
-require("dotenv").config();
-const mysql = require("mysql2");
+require('dotenv').config();
+const fs = require('fs');
+const mysql = require('mysql2');
 
-const urlDB = `mysql://${process.env.MYSQLUSER}:${process.env.MYSQLPASSWORD}@${process.env.MYSQLHOST}:${process.env.MYSQLPORT}/${process.env.MYSQLDATABASE}`;
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  port: process.env.PORT, // required for TiDB Cloud
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  ssl: {
+    ca: fs.readFileSync(process.env.CA),
+    rejectUnauthorized: true // this should be true when CA is valid
+  }
+});
 
-const connection = mysql.createConnection(urlDB);
+connection.connect((err) => {
+  if (err) {
+    console.error('❌ TiDB Cloud connection failed:', err.message);
+    return;
+  }
+  console.log('✅ Connected to TiDB Cloud!');
+});
 
 module.exports = connection;
